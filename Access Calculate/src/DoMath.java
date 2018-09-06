c747ed7f8a9bec4d03960a0d97042c5f5c4172cc
APCS2nd <APCS2nd@MHS-Dreyer-05>
2018-09-06 09:53:27 -0700

    do math

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,11 @@
+//Client code for calculate method
+//Ben Gu
+//9/6/2018
+//Version 0
+public class DoMath {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Calculate Library/Calculate.java
@@ -0,0 +1,11 @@
+//this class has math methods
+//Ben Gu
+//9/6/2018
+//Version 0
+public class Calculate {
+	
+	public int square(int number) {
+		return number*number}
+	}
+	
+}
