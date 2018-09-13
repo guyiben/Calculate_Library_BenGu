448307f9719f179851dbe0a9e10ca39bccd2eb9f
APCS2nd <APCS2nd@MHS-Dreyer-05>
2018-09-13 09:47:56 -0700

    DoMath+Calculate

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -6,8 +6,11 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		Calculate.square (-3);
-
+		System.out.println(Calculate.square(-3));
+		System.out.println(Calculate.cube(-3));
+		System.out.println(Calculate.average(4.5,5.5));
+		System.out.println(Calculate.average(4.5,5.5,6.5));
+		System.out.println(Calculate.toDegrees(3.14));
+		System.out.println(Calculate.toRadians(90));
 	}
-
 }
--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -7,5 +7,20 @@ public class Calculate {
 	public static int square(int number){
 		return number*number;
 	}
+	public static int cube(int number){
+		return number*number*number;
+	}
+	public static double average(double number1,double number2){
+		return (number1+number2)/2;
+	}
+	public static double average(double number1,double number2,double number3){
+		return (number1+number2+number3)/3;
+	}
+	public static double toDegrees(double rad){
+		return (rad/3.14159*180);
+	}
+	public static double toRadians(double deg){
+		return (deg*3.14159/180);
+	}
 	
 }
